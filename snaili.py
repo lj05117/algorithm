b8106aa34295e6baa0c4fa3d08f924d222af00da
Jiye Lee <58285947+lj05117@users.noreply.github.com>
2022-05-26 22:21:32 +0900

    Create snaili.py

---
+++ snaili.py
@@ -0,0 +1,19 @@
+N=input()
+def snail_number(number,li):
+    direction=[0,1,2,3]
+    nx=[1,0,-1,0]
+    direct_now=0
+    for i in range(number*2-1):
+        if i%4==0:
+            for j in range()
+    return li
+
+for i in range(N):
+    number=input()
+    print('#',N)
+    li=[[0 for ii in range(number)]for jj in range(number)]
+    snail_number(number,li)
+    for j in range(number):
+        for k in range(number):
+            print(li[j][k])
+        print()
