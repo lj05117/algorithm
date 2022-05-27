dd13974f52c6902863dc6979cbac4e36d5b74fb7
Jiye Lee <58285947+lj05117@users.noreply.github.com>
2022-05-27 23:49:05 +0900

    Update snaili.py

--- snaili.py
+++ snaili.py
@@ -1,19 +1,51 @@
-N=input()
-def snail_number(number,li):
-    direction=[0,1,2,3]
-    nx=[1,0,-1,0]
-    direct_now=0
-    for i in range(number*2-1):
-        if i%4==0:
-            for j in range()
-    return li
+K=int(input())
+def snali(N,li):
+    input_num=0;
+    r=0;c=0
+    while(input_num<N*N):
+        while(c<N):
+            if li[r][c]==0:
+                input_num+=1
+                li[r][c]=input_num
+                c+=1
+            else:break
+        c-=1
+        r+=1
+        while(r<N):
+            if li[r][c]==0:
+                input_num+=1
+                li[r][c]=input_num
+                r+=1
+            else:break
+        r-=1
+        c-=1
+        while(c>=0):
+            if li[r][c]==0:
+                input_num+=1
+                li[r][c]=input_num
+                c-=1
+            else:break
+        c+=1
+        r-=1
+        while(r>=0):
+            if li[r][c]==0:
+                input_num+=1
+                li[r][c]=input_num
+                r-=1
+            else:
+                break
+        r+=1
+        c+=1
 
-for i in range(N):
-    number=input()
-    print('#',N)
-    li=[[0 for ii in range(number)]for jj in range(number)]
-    snail_number(number,li)
-    for j in range(number):
-        for k in range(number):
-            print(li[j][k])
+
+
+for i in range(K):
+    N=int(input())
+    print('#',i+1)
+    li=[[0 for j in range(N)]for k in range(N)]
+    snali(N, li)
+    for n in range(N):
+        for m in range(N):
+            print(li[n][m],end=" ")
         print()
+    
