ac81f706794232926e0b5f6bf3c26c922ae78d06
Jiye Lee <58285947+lj05117@users.noreply.github.com>
2022-06-10 11:24:29 +0900

    Update sorting_max_number.py

--- Programmers/sorting_max_number.py
+++ Programmers/sorting_max_number.py
@@ -1,11 +1,4 @@
 def solution(numbers):
     numbers=list(map(str,numbers))
-    numbers=sorted(numbers,reverse=True)
-    answer=''
-    for i in range(len(numbers)-1):
-        if numbers[i][0]==numbers[i+1][0]:
-            if len(numbers[i+1])<len(numbers[i]) and numbers[i][1]<=numbers[i][0]:
-                numbers[i+1],numbers[i]=numbers[i],numbers[i+1]
-        answer+=numbers[i]
-    answer+=numbers[-1]
-    return answer
+    numbers.sort(key=lambda x :x*3 ,reverse=True)
+    return str(int(''.join(numbers)))
