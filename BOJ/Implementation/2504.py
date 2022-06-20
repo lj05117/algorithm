bca21b9945aab47367f97007dfc298a8517fe842
Jiye Lee <lj05117@naver.com>
2022-06-20 16:20:19 +0900

    2504

--- BOJ/Implementation/2504.py
+++ BOJ/Implementation/2504.py
@@ -1,37 +1,29 @@
-def two(i):
-    if len(L)>i and fail!=1:
-        if L[i]!=')':
-            if L[i]=='[':
-                three(i)
-            else:
-                fail=1
-                return 0
-def three(i):
-    if len(L)>i and fail!=1:
-        if L[i]!=']':
-            if L[i]=='(':
-                two(i)
-            else:
-                fail=1
-                return 0
-
-
-L=list(map(str, input()))
-merge=[]
-i=0
+L=list(input())
+temp=1
 sum=0
-fail=0
-while(L):
+merge=[]
+for i in range(len(L)):
     if L[i]=='(':
-        i+=1
-        two(i)
-
-    if L[i]=='[':
-        i+=1
-        three(i)
-    if fail==1:
-        print(0)
-        break
-
-if fail==0:
-    print(sum)
+        temp*=2
+        merge.append('(')
+    elif L[i]=='[':
+        temp*=3
+        merge.append('[')
+    elif L[i]==')':
+        if not merge or merge[-1]!='(':
+            sum=0
+            break
+        if L[i-1]=='(':
+            sum+=temp
+        temp//=2
+        merge.pop()
+    else:
+        if not merge or merge[-1]!='[':
+            sum=0
+            break
+        if L[i-1]=='[':
+            sum+=temp
+        temp//=3
+        merge.pop()
+if merge:print(0)
+else:print(sum)
