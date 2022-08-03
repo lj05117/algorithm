b994c217ee24c86c28d65f6b709c78aa6db7ca68
Jiye Lee <58285947+lj05117@users.noreply.github.com>
2022-08-03 14:56:58 +0900

    Create 11659.java

---
+++ BOJ/PrefixSum/11659.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine()," ");
+		int n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		int[] li=new int[n+1];
+		st=new StringTokenizer(br.readLine()," ");
+		for(int i=1;i<=n;i++) {
+			li[i]=li[i-1]+Integer.parseInt(st.nextToken());
+		}
+		for(int i=0;i<m;i++) {
+			st=new StringTokenizer(br.readLine()," ");
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
+			System.out.println(li[b]-li[a-1]);
+		}
+	}
+	
+
+}
