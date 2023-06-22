52390c9568e039ad014b02a696482c554eac43cd
lj05117 <lj05117@naver.com>
2023-06-22 18:19:49 +0900

    PG Lv2. dp 문제

---
+++ Programmers/Lv2/가장 큰 정사각형 찾기.js
@@ -0,0 +1,18 @@
+function solution(board) {
+  let answer = 0;
+  let row = board.length;
+  let col = board[0].length;
+
+  if (row < 2 || col < 2) return 1;
+
+  for (let i = 1; i < row; i++) {
+    for (let j = 1; j < col; j++) {
+      if (board[i][j] !== 0) {
+        let min = Math.min(board[i - 1][j - 1], board[i - 1][j], board[i][j - 1]);
+        board[i][j] = min + 1;
+      }
+      if (answer < board[i][j]) answer = board[i][j];
+    }
+  }
+  return answer * answer;
+}
