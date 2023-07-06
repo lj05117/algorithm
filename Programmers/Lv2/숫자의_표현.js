07025c9653c8223c27ada5e87a66847c6622a75e
lj05117 <lj05117@naver.com>
2023-07-06 10:37:26 +0900

    PG Lv2. 숫자의 표현

--- Programmers/Lv2/숫자의_표현.js
+++ Programmers/Lv2/숫자의_표현.js
@@ -1,13 +1,15 @@
 function solution(n) {
   let answer = 1;
-  for (let i = 1; i <= parseInt(n / 2); i++) {
+  for (let i = 1; i <= n / 2; i++) {
     let sum = 0;
     for (let j = i; j < n; j++) {
       sum += j;
-      if (sum >= n) break;
+      if (sum === n) {
+        answer++;
+        break;
+      }
+      if (sum > n) break;
     }
-    if (sum === n) answer++;
   }
   return answer;
 }
-console.log(solution(15));
